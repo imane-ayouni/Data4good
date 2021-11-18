9b3765cbaf961ce6f9fca8a390500dbe23d5569b
imane <imaneayouni@outlook.com>
2021-11-18 16:05:28 +0100

    in progress

--- Graphs.py
+++ Graphs.py
@@ -9,6 +9,19 @@ df.drop("genre",axis =1, inplace = True)
 df.drop("variete",axis =1, inplace = True)
 df.drop("remarquable",axis =1, inplace = True)
 print(df)
+print("Numéro d'arbres: ",len(df))
 
+df.drop(df.index[(df["circonference_cm"] == 0)], axis = 0, inplace=True)
+df.drop(df.index[(df["hauteur_m"] == 0)], axis = 0, inplace=True)
+df.drop(df.index[(df["circonference_cm"] > 470)], axis = 0, inplace=True)
+df.drop(df.index[(df["hauteur_m"] > 35 )], axis = 0, inplace=True)
+df.dropna(subset=["circonference_cm"],inplace=True)
+df.dropna(subset=["hauteur_m"],inplace=True)
 
+print("Numéro d'arbres: ",len(df))
 
+df["hauteur_m"] = 100 * df["hauteur_m"]
+
+new_df = df.rename(columns={"hauteur_m":"hauteur_cm"})
+
+print(new_df)
